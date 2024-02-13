30f7e94dcd1d79eab98c4ec6503ba1f60e0ca915
yuliakachurovska <kachurovska167kv@gmail.com>
2024-02-13 13:58:47 +0200

    add func degree of five

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-from utils import gcd
+from utils import gcd, degree_of_five
 print(gcd(8, 12))
+print(degree_of_five(125))
--- utils.py
+++ utils.py
@@ -2,3 +2,13 @@ def gcd(x, y):
     while y != 0:
         x, y = y, x % y
     return x
+
+
+def degree_of_five(numb):
+    if numb <= 0:
+        return False
+    elif numb == 1:
+        return True
+    while numb % 5 == 0:
+        numb /= 5
+    return numb == 1
