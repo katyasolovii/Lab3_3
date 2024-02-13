78a56cf0c31dfdc58a3401041ab1af5e7e262ae8
Anna Aleksandrova <anyaaleksandrova1000@gmail.com>
2024-02-13 14:00:02 +0200

    Added factorial

--- main.py
+++ main.py
@@ -1,2 +1,3 @@
-from utils import gcd
+from utils import gcd, factorial
 print(gcd(8, 12))
+print(factorial(5))
--- utils.py
+++ utils.py
@@ -2,3 +2,9 @@ def gcd(x, y):
     while y != 0:
         x, y = y, x % y
     return x
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
